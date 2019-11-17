0099f0dc2fcdf0ad3bb013a6c73b176d24a78684
Anubhav Sanyal <princebest3@rediffmail.com>
2019-11-17 16:41:54 +0530

    deleted

--- Server/WebsecServer/Login/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class UserDetails(models.Model):
-    username = models.CharField(max_length = 30)
-    password = models.CharField(max_length = 30)
-
-    def __str__(self):
-        return "Username: " + self.username + " Password: " + self.password
--- Server/WebsecServer/testing.py
+++
@@ -1,9 +0,0 @@
-import os
-p = os.path.abspath('./')
-print(p)
-p = p.split('\\')
-print(p)
-p.pop()
-print(p)
-p = '\\'.join(p)
-print(p)
